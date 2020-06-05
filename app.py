9dc0e0ecd3e43cd953298d4b218d440e01750432
Craig Nelson <34287157+supahcraig@users.noreply.github.com>
2020-06-05 11:11:56 -0500

    Update app.py
    updated to use AWS secrets to maintain the RDS credentials

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
-import sys
+#import sys
 import logging
-import rds_config
 import pymysql
 import boto3
-
-s3 = boto3.client('s3')
-
-rds_host = 'YOUR RDS ENDPOINT'  # RDS endpoint
-name = rds_config.db_username
-password = rds_config.db_password
-db_name = rds_config.db_name
+from secrets import get_secret
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+s3 = boto3.client('s3')
+
+#rds_host = 'mysqlforlambdatest.cevfxcwql3rj.us-east-1.rds.amazonaws.com'  # RDS endpoint
+rds_host = 'cnelson-rds-mysql-proxy.proxy-cevfxcwql3rj.us-east-1.rds.amazonaws.com'  # proxy endpoint
+
+secret = get_secret(secret_name='cnelson-mysql-secret', region_name='us-east-1')
+
 try:
-    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=10)
+    conn = pymysql.connect(rds_host, 
+                           user=secret['username'], 
+                           passwd=secret['password'], 
+                           db=secret['dbname'], 
+                           connect_timeout=10)
+
 except pymysql.MySQLError as e:
     logger.error('Error:  Unexpected error:  could not connect to MySQL instance.')
     logger.error(e)
-    sys.exit(99)
+    exit(99)
 
-logger.info("SUCCESS: Connection to RDS MySQL instance succeeded.")
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded.")     
+
 
 def handler(event, context):
-    logging.info('got inside the event handler')
-    
+
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
 
@@ -37,26 +42,29 @@ def handler(event, context):
     logging.info(f'The object has {len(rows)} rows.')
     # seems to end up with an extra row, not sure why
 
-    item_count = 0
+    insert_count = 0
 
     with conn.cursor() as cur:
         cur.execute('delete from players')  # clean up the table first
 
-        insert_sql = 'insert into players (player_id, year_id, team_id) values ("{}", "{}", "{}")'
+        insert_sql = 'insert into players (player_id, year_id, team_id) values (%s, %s, %s)'
 
         for row in rows:
-            logging.info('inserting a row')
             parsed_row = row.split(',')
             
             try:
-                cur.execute(insert_sql.format(parsed_row[0], parsed_row[1], parsed_row[3]))
-                item_count += 1
+                cur.execute(insert_sql, (parsed_row[0], parsed_row[1], parsed_row[3]) )
+                insert_count += 1
     
             except IndexError as e:
                 logging.error(e)
+                # let this exception pass...need to understand why there's an extra row being read
                 
 
     conn.commit()
-    logging.info(f'Added {item_count} items to RDS MySQL table.')
+    logging.info(f'Added {insert_count} items to RDS MySQL table.')
 
     return 200
+
+
+
